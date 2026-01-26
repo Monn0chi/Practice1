392f55492e4713dd83c6d4de6e63fbf7fa93efde
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-01-26 09:32:06 +0500

    Add strings.py file

---
+++ python-basics/strings.py
@@ -0,0 +1,27 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+
+#slicing 
+b = "Hello, World!"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+
+#you can modify strings
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.replace("H", "J"))
+print(a.split(","))
+print(a.strip()) 
+
+#concatenate
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
