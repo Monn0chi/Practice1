877b61cc8a1378832e0d03d9b6e495d7d04d3a8a
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-01-26 09:27:38 +0500

    Add casting.py file

---
+++ python-basics/casting.py
@@ -0,0 +1,17 @@
+x = input("Enter a number: ")
+y = int(x)
+print(type(y))
+
+
+y = float(x)
+print(type(y))
+
+
+x = float(1)     
+y = float(2.8)   
+z = float("3")   
+w = float("4.2") 
+print(x)
+print(y)
+print(z)
+print(w)
