eabab15a8606480bd94e98ae25a0f589fc57d6e7
Beksultan <147271970+Monn0chi@users.noreply.github.com>
2026-01-29 09:49:12 +0500

    Add numbers.py, data_types.py, variables.py

---
+++ python-basics/variables.py
@@ -0,0 +1,26 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
